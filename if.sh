23430b7f80559897d43bb19e81825e6fc704b17a
Asif <asifshaikh496@gmail.com>
2024-02-12 11:09:58 +0000

    Added feature2.2 in development branch

--- if.sh
+++ if.sh
@@ -13,5 +13,10 @@ fi
 
 echo "My name is $name"
 echo "I have passed in exam "#Added feature2.1 in development branch
+<<<<<<< HEAD
 echo "I have scored $marks"#Feature 2 is completed and ready for release
 echo "I have made some changes"#
+=======
+
+
+>>>>>>> 6726935 (Added feature2.2 in development branch)
