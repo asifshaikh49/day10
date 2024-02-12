3d310861d61f73224b82b35c8679727a825d6d8e
Asif <asifshaikh496@gmail.com>
2024-02-12 11:04:16 +0000

    This is the bug fix in development branch

---
+++ if.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter your name : " name
+echo "$name"
+read -p "Enter your marks : " marks
+if [[ $marks -gt 40 ]]
+then
+	echo "Your are pass "
+else
+	echo "Your are fail"
+	echo "Do Study properly "
+fi
+
+echo "My name is $name"
+
